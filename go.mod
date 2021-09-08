d362ec1c0f70876d0f09705b7b5e7d5dde5a6007
黄兵 <1278032416@qq.com>
2021-09-08 08:40:14 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-gorm-samples
+
+go 1.16
+
+require (
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.14 // indirect
+)
---
+++ user.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+	"math/rand"
+)
+
+func (u User) CreateTable() {
+
+	// 迁移 schema
+	err := db.AutoMigrate(&u)
+	if err != nil {
+		panic("auto migrate failed:%v")
+	}
+}
+
+func (u User) Insert() {
+	user := User{Name: "test", Age: 18}
+	result := db.Create(&user)
+	fmt.Printf("受影响的行数: %d\n", result.RowsAffected)
+	fmt.Printf("%v\n", user)
+}
+
+// InsertBatch 批量插入
+func (u User) InsertBatch() {
+	names := []string{"小红", "小黑", "小南", "晓楠", "小白", "晶晶", "晓晓", "沐沐", "白小仙"}
+	var users []User
+	for i := 0; i < 20; i++ {
+
+		users = append(users, User{Name: names[rand.Intn(len(names))], Age: uint(rand.Intn(200))})
+	}
+	db.CreateInBatches(users, len(users))
+}
+
+// FindByName 通过name查询
+func (u User) FindByName(name string) []User {
+	var user []User
+	db.Where(`name like ?`, name+"%").Find(&user)
+	return user
+}
+
+// Page 根据当前页与大小查询
+func (u User) Page(currentPage, countPage int) []User {
+	var user []User
+	db.Offset(currentPage).Limit(countPage).Find(&user)
+	return user
+}
+
+// PageCount 统计
+func (u User) PageCount() int64 {
+	var count int64
+	db.Model(&u).Count(&count)
+	return count
+}
+
+// TxRollback 嵌套事务测试回滚
+func (u User) TxRollback(id uint) bool {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		tx.Where("id = ?", id).Delete(&u)
+		return errors.New("test transaction rollback")
+	})
+	if err != nil {
+		return true
+	}
+	return false
+
+}
+
+type User struct {
+	ID   uint   `gorm:"column:id;not null;type:uint;primaryKey;autoIncrement;comment:'id'"`
+	Name string `gorm:"column:name;not null;type:varchar(32);comment:'名称';index"`
+	Age  uint   `gorm:"column:age;type:uint;comment:'年龄'"`
+}
+
+//func (u User) TableName() string {
+//
+//}
