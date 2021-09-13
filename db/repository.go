ea3333e725318859f9ad3df0ddcc9f11fadf513a
黄兵 <1278032416@qq.com>
2021-09-13 11:32:48 +0800

    fixed

--- db/repository.go
+++ db/repository.go
@@ -1,39 +1,70 @@
 package db
 
 import (
-	"go-gorm-samples/conditions"
 	"gorm.io/gorm"
 )
 
-type Mapper interface {
-	Insert(value interface{}) (int64, error)
-	Select(wrapper conditions.Wrapper, value interface{}) (int64, error)
-	Find(value IModel) (int64, error)
-	FindArray(value interface{}) (int64, error)
-}
+// BaseMapper 实现 Mapper
 type BaseMapper struct {
 	DB *gorm.DB
 }
 
-func (mapper *BaseMapper) Insert(value interface{}) (int64, error) {
+func NewBaseMapper(db *gorm.DB) *BaseMapper {
+	return &BaseMapper{DB: db}
+}
+
+func (mapper *BaseMapper) Insert(value IModel) (int64, error) {
 	result := mapper.DB.Create(value)
 	return result.RowsAffected, result.Error
 }
 
-func (mapper BaseMapper) Select(wrapper conditions.Wrapper, value interface{}) (int64, error) {
-	expressions := wrapper.GetExpressions()
-	for _, expression := range expressions {
-		mapper.DB.Statement.AddClause(expression)
-	}
-	result := mapper.DB.Find(value)
+func (mapper *BaseMapper) InsertBatch(values interface{}, len int) (int64, error) {
+	result := mapper.DB.CreateInBatches(values, len)
 	return result.RowsAffected, result.Error
 }
-func (mapper BaseMapper) Find(value IModel) (int64, error) {
-	result := mapper.DB.Take(value)
+func (mapper *BaseMapper) DeleteById(id interface{}, model IModel) (int64, error) {
+	result := mapper.DB.Where(model.IdColumnName()+"= ?", id).Delete(model)
+	return result.RowsAffected, result.Error
+}
+func (mapper *BaseMapper) DeleteBatchIds(model IModel, id ...interface{}) (int64, error) {
+	result := mapper.DB.Where(model.IdColumnName()+" in (?)", id).Delete(model)
+	return result.RowsAffected, result.Error
+}
+func (mapper *BaseMapper) DeleteByMap(params map[string]interface{}, model IModel) (int64, error) {
+	var db *gorm.DB = mapper.DB
+	for k, v := range params {
+		db = db.Where(k+" = ?", v)
+	}
+	result := db.Delete(model)
 	return result.RowsAffected, result.Error
 }
 
-func (mapper BaseMapper) FindArray(value interface{}) (int64, error) {
-	result := mapper.DB.Find(value)
+func (mapper *BaseMapper) UpdateById(id interface{}, value IModel) (int64, error) {
+	result := mapper.DB.Where(value.IdColumnName()+" = ?", id).Updates(value)
+	return result.RowsAffected, result.Error
+}
+func (mapper *BaseMapper) Update(value IModel, params map[string]interface{}) (int64, error) {
+	db := mapper.DB
+	for k, v := range params {
+		db = db.Where(k+"= ?", v)
+	}
+	result := db.Updates(value)
+	return result.RowsAffected, result.Error
+}
+func (mapper *BaseMapper) SelectById(id interface{}, model IModel) (int64, error) {
+	result := mapper.DB.Where(model.IdColumnName()+" = ?", id).Find(model)
+	return result.RowsAffected, result.Error
+}
+func (mapper *BaseMapper) SelectBatchIds(model interface{}, idName string, id ...interface{}) (int64, error) {
+	result := mapper.DB.Where(idName+" in (?)", id).Find(model)
+	return result.RowsAffected, result.Error
+}
+
+func (mapper *BaseMapper) SelectByMap(model interface{}, params map[string]interface{}) (int64, error) {
+	var db = mapper.DB
+	for k, v := range params {
+		db = db.Where(k+" = ?", v)
+	}
+	result := db.Find(model)
 	return result.RowsAffected, result.Error
 }
--- db/repository_test.go
+++ db/repository_test.go
@@ -2,46 +2,109 @@ package db
 
 import (
 	"fmt"
-	"go-gorm-samples/conditions"
 	"testing"
 )
 
 func TestBaseMapper_Insert(t *testing.T) {
-	mapper := BaseMapper{DB: DB}
-	mapper.Insert(&User{Name: "测试1", Age: 19})
-	mapper.Insert(&User{Name: "测试2", Age: 20})
-	mapper.Insert(&User{Name: "测试3", Age: 30})
-}
-func TestBaseMapper_Select(t *testing.T) {
-	mapper := BaseMapper{DB: DB}
-	wrapper := conditions.NewWrapper()
-	wrapper.Func = wrapper.OrderByDesc("age", "name")
-	wrapper.Compare = wrapper.Eq("id", 1).Ne("name", "cc")
-	users := []User{}
-	mapper.Select(wrapper, &users)
-	fmt.Printf("%v", users)
-
+	value := User{Name: "insert_test", Age: 18}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.Insert(&value)
+	if err != nil {
+		fmt.Printf("Inser error:%v", err)
+		return
+	}
+	fmt.Printf("%v", value)
 }
 
-func TestBaseMapper_Select_between(t *testing.T) {
-	mapper := BaseMapper{DB: DB}
-	wrapper := conditions.NewWrapper()
-	wrapper.Compare = wrapper.Between("id", 1, 3)
-	users := []User{}
-	mapper.Select(wrapper, &users)
-	fmt.Printf("%v", users)
+func TestBaseMapper_InsertBatch(t *testing.T) {
+	var values []User
+	for i := 0; i < 3; i++ {
+		values = append(values, User{Name: fmt.Sprintf("Insert_Batch_%d", i), Age: 18 + i})
+	}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.InsertBatch(values, len(values))
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", values)
 }
 
-func TestBaseMapper_Find(t *testing.T) {
-	var user User
-	mapper := BaseMapper{DB: DB}
-	mapper.Find(&user)
-	fmt.Printf("%v", user)
+func TestBaseMapper_DeleteById(t *testing.T) {
+	value := User{}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.DeleteById(1, &value)
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", value)
 }
 
-func TestBaseMapper_FindArray(t *testing.T) {
-	var users []User
-	mapper := BaseMapper{DB: DB}
-	mapper.FindArray(users)
-	fmt.Printf("%v", users)
+func TestBaseMapper_DeleteBatchIds(t *testing.T) {
+	value := User{}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.DeleteBatchIds(&value, 2, 3)
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", value)
+}
+
+func TestBaseMapper_DeleteByMap(t *testing.T) {
+	params := map[string]interface{}{"id": 4, "age": 20}
+	value := User{}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.DeleteByMap(params, &value)
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", value)
+}
+
+func TestBaseMapper_UpdateById(t *testing.T) {
+	user := User{Name: "update_id", Age: 19}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.UpdateById(1, &user)
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", user)
+}
+
+func TestBaseMapper_Update(t *testing.T) {
+	user := User{Name: "update_id", Age: 40}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.Update(user, map[string]interface{}{"id": 1, "age": 19})
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", user)
+}
+
+func TestBaseMapper_SelectById(t *testing.T) {
+	value := User{}
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.SelectById(1, &value)
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", value)
+}
+
+func TestBaseMapper_SelectBatchIds(t *testing.T) {
+	var values []User
+	//var values = make([]User, 0)
+	mapper := NewBaseMapper(DB)
+	_, err := mapper.SelectBatchIds(&values, User{}.IdColumnName(), 1, 2)
+	if err != nil {
+		fmt.Printf("Insert Batch error:%v", err)
+		return
+	}
+	fmt.Printf("%#v", values)
 }
